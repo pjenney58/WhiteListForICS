624842b9036d77e97d0864a769fd7490e006d89f
Pete Jenney <pete.jenney@gmail.com>
2022-06-27 09:23:36 -0400

    Cleaned up and added current license

--- rmdriver/legacy/kprobe_rmescape_test 2.c
+++ rmdriver/legacy/kprobe_rmescape_test 2.c
@@ -1,3 +1,23 @@
+/******************************************************************************
+ * Aerolock whitelisting reference code for industiral control systems (ICS)
+ * Copyright (c) 2012-2022, by Resilient Machines, LLC and Sand Drft Software, LLC, and Peter H. Jenney
+ * (r) All rights reserved. 
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ *****************************************************************************/
 
 #include <linux/kernel.h>
 #include <linux/module.h>
